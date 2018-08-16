7bb6a2325e4e54c295b811aa5336302e0b2be8be
程嘉丰 <cheng.jiafeng@seatell.cn>
2018-08-16 15:54:05 +0800

    udpate

--- src/components/child.js
+++ src/components/child.js
@@ -1,16 +1,38 @@
 import React, {Component} from 'react'
-
+import ReactSwiper from 'reactjs-swiper';
 export default class Child extends Component {
     constructor(props) {
         super(props);
     }
 
     render() {
-        const arr = [12, 32, 4, 5, 2323, 55, 333, 2];
+        const items = [{
+            image: 'http://alloyteam.github.io/AlloyTouch/example/asset/ci1.jpg',
+            title: '图片1',
+            link: 'http://jd.com'
+        }, {
+            image: 'http://alloyteam.github.io/AlloyTouch/example/asset/ci2.jpg',
+            title: '图片2',
+        }, {
+            image: 'http://alloyteam.github.io/AlloyTouch/example/asset/ci3.jpg',
+            title: '图片3',
+            link: 'http://jd.com'
+        }, {
+            image: 'http://alloyteam.github.io/AlloyTouch/example/asset/ci4.jpg',
+            title: '图片4',
+        }];
+
+        const swiperOptions = {
+            preloadImages: true,
+            autoplay: 4000,
+            autoplayDisableOnInteraction: false,
+            direction:'vertical'
+        };
         return (
             <div>
-                <h1>数组是:{arr.join(',')}</h1>
-                <h1>最大值为:{Math.max.apply(null, arr)}</h1>
+                <ReactSwiper swiperOptions={swiperOptions} showPagination items={items}
+                             className="swiper-example" />
+                );
             </div>
         )
     }
--- src/components/home.js
+++ src/components/home.js
@@ -1,15 +1,72 @@
 import React, {Component} from 'react'
-import '../css/home1.css'
+import Swiper from 'swiper/dist/js/swiper.js'
+
+import 'swiper/dist/css/swiper.css'
+import '../css/home.scss'
 import img from '../images/2.png'
-export default class Home extends Component {
+
+class Home extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+
+        }
+    }
+
+    componentDidMount() {
+        new Swiper('.wxchat-banner', {
+            direction: 'vertical',
+            observer: true,
+            // pagination: {
+            //     el: '.swiper-pagination',
+            // },
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+        });
+    }
+
     render() {
+        const items = [img, img, img];
         return (
-            <div>
-                <h1 className='aaa'>Home</h1>
-                <h1>Home</h1>
-                <h1>Home</h1>
-                <img src={img} />
+            <div className="wxchat-banner">
+                <div className="swiper-wrapper panel_div">
+                    <div className="swiper-slide" style={{background:'#08ee65'}}>Slide 1</div>
+                    <div className="swiper-slide" style={{background:'#8fceff'}}>Slide 2</div>
+                    <div className="swiper-slide" style={{background:'#c7d6e5'}}>Slide 3</div>
+                </div>
+                <div className="swiper-button-prev"></div>
+                <div className="swiper-button-next"></div>
             </div>
         )
     }
 }
+
+export default Home
+
+// export default class Home extends Component {
+//     constructor(props) {
+//         super(props)
+//         this.state = {
+//             list: [1, 2, 3]
+//         }
+//     }
+//
+//     componentDidMount() {
+//
+//     }
+//
+//     render() {
+//
+//         let {list} = this.state
+//         return (
+//             <div>
+//                 {
+//                     list.length>0? <Child list={list} /> : null
+//                 }
+//             </div>
+//
+//         )
+//     }
+// }
