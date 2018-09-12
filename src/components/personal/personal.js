b332dbf8874882b5dd54744c614b937975a5086d
JabFury <chengjiafeng@hotmail.com>
2018-09-12 10:27:47 +0800

    update

--- src/components/personal/personal.js
+++ src/components/personal/personal.js
@@ -13,7 +13,7 @@ class Personal extends Component {
                 <div className='inform_div'>
 
                     <div className='introduce'>
-                        <p style={{fontWeight:'lighter',marginBottom:'3vh',fontSize:'40px'}}>我是程嘉丰</p>
+                        <p style={{fontWeight:'lighter',marginBottom:'3vh',fontSize:'35px'}}>我是程嘉丰</p>
                         <p>一个22岁的前端攻城狮</p>
                         <p>篮球 乒乓球 音乐</p>
                         <p>追求新鲜事物，享受美妙视觉感悟</p>
